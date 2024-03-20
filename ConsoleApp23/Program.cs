d1cb8bbf01b56861c51303e3abf03e36e5f83535
Victoria Portnaya <150618993+victoriaportnaya@users.noreply.github.com>
2024-03-20 12:35:52 +0200

    Initial commit

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,16 @@
+﻿// read text file 
+// ceeate dict
+// add frequency of the words to dict 
+
+using System.Net.Mime;
+// create dict 
+class Program
+{
+    static void Main()
+    {
+        string myText = File.ReadAllText("textFile.txt");
+        Dictionary<char, int> charFrequency = myText
+            .GroupBy(c => c)
+            .ToDictionary(g => g.Key, g => g.Count());
+    }
+}
